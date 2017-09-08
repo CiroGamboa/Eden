5b970fc6de522f26f9581434f164545624e14ea9
unknown <admin@I201-008.bga.upb>
2017-09-08 16:18:13 -0500

    Creación de clases

--- eden/eden/settings.py
+++ eden/eden/settings.py
@@ -37,6 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'fotos',
+    'libros',
+    'libroUser',
+    'LogHumedad',
+    'LogTemperatura'
+    ,'LogValvula'
+    ,'Maceta'
+    ,'Planta',
+    'User',
 ]
 
 MIDDLEWARE = [
